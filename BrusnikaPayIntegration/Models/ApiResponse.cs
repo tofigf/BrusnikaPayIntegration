90282fd8ef8d715c6183be9d1d19d5298d2ae320
Tofig Farajullayev <tofiq.ferecullayev@gmail.com>
2025-08-23 00:01:55 +0400

    Add project files.

---
+++ BrusnikaPayIntegration/Models/ApiResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrusnikaPayIntegration.Models
+{
+    public class ApiResponse<T>
+    {
+        public ApiResult Result { get; set; } = new();
+        public T? Data { get; set; }
+        public int TotalNumberRecords { get; set; }
+    }
+}
